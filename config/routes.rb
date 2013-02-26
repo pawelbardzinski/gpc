36e0febb90eed24fad551c117d977083d83785d4
cx <droopsik@gmail.com>
2013-02-26 21:29:10 +0100

    sortownie

--- config/routes.rb
+++ config/routes.rb
@@ -65,7 +65,5 @@ Sh1nyV1::Application.routes.draw do
   match 'topics/list' => 'topics#list', :as => :topics
 #  resources :topics
   match ':controller(/:action(/:id(.:format)))'
-
-	match 'crossdomain.xml' => 'topics#crossdomain', :as => :topics
 	
 end
