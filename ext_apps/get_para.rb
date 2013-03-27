a919560b4ae1919d640616fcf70d2e10db8496cb
pawelbardzinski <pawel.bardzinski@gmail.com>
2013-03-27 19:14:45 +0100

    deleting unused files

--- ext_apps/get_para.rb
+++
@@ -1,9 +0,0 @@
-require 'readability'
-require 'open-uri'
-require 'nokogiri'
-
-source = open(ARGV[0]).read
-str = Readability::Document.new(source).content
-@content = Nokogiri::HTML(str).text.strip!.slice(0,300).gsub!("\n"," ").gsub!("\t"," ") + '...'
-
-puts @content
